a517fb49ea46ec280ddc778b19bb32b877f28ba8
Alexandre Oliveira <alexandreoliveira@MacBookAir.local>
2023-12-11 10:54:05 -0300

    read json file js

---
+++ js/file.js
@@ -0,0 +1,50 @@
+export const jsonData = [
+    {
+        "id": 10,
+        "title": "Introdução",
+        "lessons": [
+            {
+                "id": 54,
+                "title": "Como funciona o curso",
+                "media": "f60b0450-96d0-4244-8e4f-bde367b21bfc",
+                "timestamp": "2023-12-06T16:34:56Z"
+            },
+            {
+                "id": 74,
+                "title": "Orientações importantes",
+                "media": "sadf876f-8d7f-2864-d6f6-89sad7f7lads",
+                "timestamp": "2023-12-02T17:45:00Z"
+            },
+            {
+                "id": 65,
+                "title": "O que você vai aprender",
+                "media": "lklk1213-92j3-9747-9o47-dkljjasd98ff",
+                "timestamp": "2023-12-08T09:20:00Z"
+            }
+        ]    
+    },
+    {
+        "id": 23,
+        "title": "Desenvolvendo o projeto",
+        "lessons": [
+            {
+                "id": 38,
+                "title": "Criando o projeto",
+                "media": "kjhfjh28-2864-2836-hf76-8d8f8dfhfdf",
+                "timestamp": "2023-12-04T08:00:00Z"
+            },
+            {
+                "id": 149,
+                "title": "Implementando as funcionalidades",
+                "media": "djheruyr3-8fjy-8276-47kh-kdfhhfi273",
+                "timestamp": "2023-12-09T08:25:30Z"
+            },
+            {
+                "id": 61,
+                "title": "Testando tudo",
+                "media": "kdjf9274df-43df-9874-df55-teowncr846",
+                "timestamp": "2023-12-10T10:35:00Z"
+            }
+        ]
+    }
+];
